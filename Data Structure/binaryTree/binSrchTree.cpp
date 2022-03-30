6f2b7a458da60fbcb074738d8cfb7c422900e313
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-30 16:09:01 +0900

    Rename binaryTree/binSrchTree.cpp to Data Structure/binaryTree/binSrchTree.cpp

---
+++ Data Structure/binaryTree/binSrchTree.cpp
@@ -0,0 +1,52 @@
+#include "binSrchTree.h"
+/*
+void main() {
+	binSrchTree tree;
+
+	// 삽입 연산 테스트
+	tree.insert(new binaryNode(35));
+	tree.insert(new binaryNode(18));
+	tree.insert(new binaryNode(7));
+	tree.insert(new binaryNode(26));
+	tree.insert(new binaryNode(12));
+	tree.insert(new binaryNode(3));
+	tree.insert(new binaryNode(68));
+	tree.insert(new binaryNode(22));
+	tree.insert(new binaryNode(30));
+	tree.insert(new binaryNode(99));
+
+	// 트리 기본 정보
+	printf("노드의 개수 = %d\n", tree.getCount());
+	printf("단말의 개수 = %d\n", tree.getLeafCount());
+	printf("트리의 높이 = %d\n", tree.getHeight());
+
+	// 여러 가지 방법의 순회를 통한 노드 정보 출력
+	tree.inorder();
+	tree.preorder();
+	tree.postorder();
+	tree.levelorder();
+
+	// 노드 탐색
+	tree.search(26);
+	tree.search(25);
+
+	// 삭제 연산
+	printf("case 1 ==> 노드 3 삭제");
+	tree.remove(3);
+	tree.levelorder();
+	printf("case 2 ==> 노드 68 삭제");
+	tree.remove(68);
+	tree.levelorder();
+	printf("case 3 ==> 노드 18 삭제");
+	tree.remove(18);
+	tree.levelorder();
+	printf("case 4 ==> 노드 35 삭제(루트 노드 삭제)");
+	tree.remove(35);
+	tree.levelorder();
+
+	// 최종 트리 정보 출력
+	printf("노드의 개수 = %d\n", tree.getCount());
+	printf("단말의 개수 = %d\n", tree.getLeafCount());
+	printf("트리의 높이 = %d\n", tree.getHeight());
+}
+*/
