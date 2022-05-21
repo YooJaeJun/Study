caca2f95c359f829eb554badae74a6a903759aad
YooJaeJun <zoflqldks123@gmail.com>
2022-05-21 18:41:33 +0900

    Update 마우스 매크로.cpp

--- 프로젝트/마우스 매크로.cpp
+++ 프로젝트/마우스 매크로.cpp
@@ -2,6 +2,11 @@
 #include <cstdio>
 
 
+#define RECORD VK_F2	// 녹화
+#define STOP VK_F3		// 정지
+#define START VK_F4		// 시작
+#define EXIT VK_F5		// 종료
+
 POINT pt;
 const int limit = 40000;
 int x[limit];
@@ -10,7 +15,7 @@ int sub_x[limit], sub_y[limit];
 int sub_x2[limit], sub_y2[limit];
 int check_mouse;
 int check_mouse2;
-int i;
+int frame;
 bool is_end;
 
 void home();
@@ -21,18 +26,18 @@ void play();
 int main() {
 	check_mouse = 2;
 	check_mouse2 = 2;
-	i = 0;
-	system("mode con: cols=30 lines=13");
+	frame = 0;
+	system("mode con: cols=35 lines=13");
 	home();
-	while (true) {
-		if (GetKeyState(VK_F10) < 0) {
+	while (1) {
+		if (GetKeyState(EXIT) < 0) {
 			quit();
 			return 0;
 		}
-		else if (GetKeyState(VK_F3) < 0) {
+		else if (GetKeyState(RECORD) < 0) {
 			recording();
 		}
-		else if (GetKeyState(VK_F8) < 0) {
+		else if (GetKeyState(START) < 0) {
 			play();
 		}
 
@@ -40,7 +45,7 @@ int main() {
 			return 0;
 		}
 
-		Sleep(10); //Sleep(1000)은 1초임. 즉, 0.01초를 기다린후 위로 올라가 반복문을 다시 실행 
+		Sleep(10); // 0.01초
 	}
 	return 0;
 }
@@ -50,10 +55,10 @@ void home() {
 	puts("");
 	puts("");
 	puts("	-----------------");
-	puts("	|   F3 : 녹화   |");
-	puts("	|   F7 : 정지   |");
-	puts("	|   F8 : 시작   |");
-	puts("	|   F10 : 종료  |");
+	puts("	|   F2 : 녹화   |");
+	puts("	|   F3 : 정지   |");
+	puts("	|   F4 : 시작   |");
+	puts("	|   F5 : 종료   |");
 	puts("	-----------------");
 }
 
@@ -65,48 +70,48 @@ void quit() {
 void recording() {
 	printf("마우스 입력중입니다.\n");
 
-	for (i = 0; i != limit; i++) {
-		x[i] = 0;
-		y[i] = 0;
-		sub_x[i] = 0;
-		sub_y[i] = 0;
-		sub_x2[i] = 0;
-		sub_y2[i] = 0;
+	for (frame = 0; frame != limit; frame++) {
+		x[frame] = 0;
+		y[frame] = 0;
+		sub_x[frame] = 0;
+		sub_y[frame] = 0;
+		sub_x2[frame] = 0;
+		sub_y2[frame] = 0;
 	}
-	i = 0;
+	frame = 0;
 
 	while (true) {
 		GetCursorPos(&pt);
-		x[i] = pt.x;
-		y[i] = pt.y;
-		sub_x[i] = 0;	// 왼쪽버튼 누르고 떼는 모습 저장하는 곳
-		sub_y[i] = 0;
-		i++;	//i는 프레임을 의미
+		x[frame] = pt.x;
+		y[frame] = pt.y;
+		sub_x[frame] = 0;	// 왼쪽버튼 누르고 떼는 모습 저장하는 곳
+		sub_y[frame] = 0;
+		frame++;	// 프레임을 의미
 		Sleep(10);
 		if (GetKeyState(VK_LBUTTON) < 0
 			&& check_mouse >= 1
 			|| GetKeyState(VK_RBUTTON) < 0
-			&& check_mouse2 >= 1) {	// 왼쪽버튼, 오른쪽 버튼  눌렀을 때
+			&& check_mouse2 >= 1) {	// 왼쪽버튼, 오른쪽 버튼 눌렀을 때
 
 			if (GetKeyState(VK_LBUTTON) < 0
 				&& check_mouse >= 1) {
 				//왼쪽만 
-				sub_x[i] = x[i - 1];
-				sub_y[i] = y[i - 1];
-				x[i] = 5000;
-				y[i] = 5000;
+				sub_x[frame] = x[frame - 1];
+				sub_y[frame] = y[frame - 1];
+				x[frame] = 5000;
+				y[frame] = 5000;
 				check_mouse = 0;
 			}
 			else if (GetKeyState(VK_RBUTTON) < 0
 				&& check_mouse2 >= 1) {
 				//오른쪽만 
-				sub_x2[i] = x[i - 1];
-				sub_y2[i] = y[i - 1];
-				x[i] = 7000;
-				y[i] = 7000;
+				sub_x2[frame] = x[frame - 1];
+				sub_y2[frame] = y[frame - 1];
+				x[frame] = 7000;
+				y[frame] = 7000;
 				check_mouse2 = 0;
 			}
-			i++;
+			frame++;
 		}
 		else if (GetKeyState(VK_LBUTTON) >= 0
 			&& check_mouse == 0
@@ -117,29 +122,29 @@ void recording() {
 			if (GetKeyState(VK_LBUTTON) >= 0
 				&& check_mouse == 0) {
 
-				sub_x[i] = x[i - 1];
-				sub_y[i] = y[i - 1];
-				x[i] = 6000;
-				y[i] = 6000;
+				sub_x[frame] = x[frame - 1];
+				sub_y[frame] = y[frame - 1];
+				x[frame] = 6000;
+				y[frame] = 6000;
 				check_mouse = 1;
 			}
 			else if (GetKeyState(VK_RBUTTON) >= 0
 				&& check_mouse2 == 0) {
 
-				sub_x2[i] = x[i - 1];
-				sub_y2[i] = y[i - 1];
-				x[i] = 8000;
-				y[i] = 8000;
+				sub_x2[frame] = x[frame - 1];
+				sub_y2[frame] = y[frame - 1];
+				x[frame] = 8000;
+				y[frame] = 8000;
 				check_mouse2 = 1;
 			}
-			i++;
+			frame++;
 		}
-		if (GetKeyState(VK_F7) < 0) {
-			printf("녹화를 멈춥니다.\n");
+		if (GetKeyState(STOP) < 0) {
+			printf("녹화를 정지합니다.\n");
 			break;
 		}
 
-		if (GetKeyState(VK_F10) < 0) {
+		if (GetKeyState(EXIT) < 0) {
 			quit();
 			break;
 		}
@@ -148,43 +153,43 @@ void recording() {
 
 void play() {
 	printf("재생중입니다. \n");
-	i = 0; // 프레임 처음으로 돌아가서 재생하기 위한 n
+	frame = 0; // 프레임 처음으로 돌아가서 재생하기 위한 n
 
 	while (true) {
-		switch (x[i]) {
+		switch (x[frame]) {
 		case 7000:	// 왼쪽버튼 눌러지는 것을 5000이라고 가정
-			mouse_event(MOUSEEVENTF_RIGHTDOWN, sub_x2[i], sub_y2[i], 0, 0); // 좌표에 맞게 왼쪽버튼 누르기
+			mouse_event(MOUSEEVENTF_RIGHTDOWN, sub_x2[frame], sub_y2[frame], 0, 0); // 좌표에 맞게 왼쪽버튼 누르기
 			Sleep(1);
 			break;
 		case 8000:	// 왼쪽버튼 떼지는 것을 6000이라고 가정
-			mouse_event(MOUSEEVENTF_RIGHTUP, sub_x2[i], sub_y2[i], 0, 0);  // 좌표에 맞게 왼쪽버튼 떼기
+			mouse_event(MOUSEEVENTF_RIGHTUP, sub_x2[frame], sub_y2[frame], 0, 0);  // 좌표에 맞게 왼쪽버튼 떼기
 			Sleep(1);
 			break;
 		case 5000:	// 왼쪽버튼 눌러지는 것을 5000이라고 가정
-			mouse_event(MOUSEEVENTF_LEFTDOWN, sub_x[i], sub_y[i], 0, 0); // 좌표에 맞게 왼쪽버튼 누르기
+			mouse_event(MOUSEEVENTF_LEFTDOWN, sub_x[frame], sub_y[frame], 0, 0); // 좌표에 맞게 왼쪽버튼 누르기
 			Sleep(1);
 			break;
 		case 6000:	// 왼쪽버튼 떼지는 것을 6000이라고 가정
-			mouse_event(MOUSEEVENTF_LEFTUP, sub_x[i], sub_y[i], 0, 0);  // 좌표에 맞게 왼쪽버튼 떼기
+			mouse_event(MOUSEEVENTF_LEFTUP, sub_x[frame], sub_y[frame], 0, 0);  // 좌표에 맞게 왼쪽버튼 떼기
 			Sleep(1);
 			break;
 		}
 
-		if (x[i] == 0) { //NULL을 만나면 작동
-			i = 0; // 즉, 매크로의 끝부분으로 가면 다시 n = 0 으로 하여 다시 처음부터 시작
+		if (x[frame] == 0) { //NULL을 만나면 작동
+			frame = 0; // 즉, 매크로의 끝부분으로 가면 다시 n = 0 으로 하여 다시 처음부터 시작
 		}
 
-		SetCursorPos(x[i], y[i]); // 0.01초마다 지정한 곳의 좌표로 마우스 이동
-		Sleep(10);//0.01초마다 한번씩 쉬고 
+		SetCursorPos(x[frame], y[frame]); // 0.01초마다 지정한 곳의 좌표로 마우스 이동
+		Sleep(10); // 0.01초 
 
-		i++;
+		frame++;
 
 		//0.01초마다 조건문 확인 
-		if (GetKeyState(VK_F7) < 0) {
+		if (GetKeyState(STOP) < 0) {
 			printf("재생을 멈춥니다.\n");
 			break;
 		}
-		if (GetKeyState(VK_F10) < 0) {
+		if (GetKeyState(EXIT) < 0) {
 			quit();
 			break;
 		}
